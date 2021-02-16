d112df85f4ffaccfb2260c152f43bcd240f7a093
kirit-pendyala <pendyalakirit@gmail.com>
2021-02-16 10:52:37 -0600

    Sol

---
+++ Warehouse Manager/Sol.sql
@@ -0,0 +1,6 @@
+select name as warehouse_name, sum(units * vol) as volume
+from Warehouse w
+join (select product_id, Width*Length*Height as vol
+     from Products) p
+on w.product_id = p.product_id
+group by name
